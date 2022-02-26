abe44e07b3b7ed7001aaf46f3a672c19624af12c
prilandin210221 <100016090+prilandin210221@users.noreply.github.com>
2022-02-26 16:11:42 +0700

    Delete 2_Versioning Control With Git directory

--- 2_Versioning Control With Git/Praktikum/example.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// ini adalah kode yang digunakan untuk
-// membuat sebuah jembatan yang kokoh
-
-// jembatan ini dapat diandalkan untuk
-// menghubungkan satu wilayah yang terpisah sungai
